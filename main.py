7d257fb26ac7e1ec91bf168f285797c03ab16ca8
Alena Choong <roxalena1994@gmail.com>
2018-01-05 15:48:58 +1300

    Don't know why but it wants me to commit this

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import socket
 import re
 import urllib.request
 import config
+import time
 
 def send_message(socket, message):
     socket.send(('PRIVMSG #' + config.CHAN + ' :' + message + '\r\n').encode())
@@ -15,19 +16,40 @@ def get_search(pattern, text):
     same = re.search(pattern, text)
     return None if same == None else same.group(0)
 
+start_time = time.time()
+
+def uptime():
+    #diff = time.gmtime(time.time() - start_time)
+    uptime = "Leeny has been streaming for "
+
+    minutes, seconds = divmod(int(time.time() - start_time), 60)
+    hours, minutes = divmod(minutes, 60)
+
+    if (hours == 0):
+        if (minutes == 0):
+            return uptime + f'{seconds} seconds.'
+        else:
+            return uptime + f'{minutes} minutes and {seconds} seconds.'
+    else:
+        return uptime + f'{hours} hours, {minutes} minutes and {seconds} seconds.'
+    #return time.strftime("Leeny has been streaming for %H hours, %M minutes and %S seconds.", time.gmtime(time.time() - start_time))
+
 def commands(x):
     return {
-        'commands': '!coolkids | !discord | !elo | !girth | !overkill | !sens | !social | !yusss',
-        'coolkids': 'Dane [twitch.tv/dhQk] | Jordan [twitch.tv/IncredFruityTV] | Leon [twitch.tv/LeoniLive] | Mitchell [twitch.tv/SenOokami] | MK [twitch.tv/Zexy_DemoN] | Rag [twitch.tv/Ragnarok1stx] | Sam [twitch.tv/Amorisaiya] | Sav [twitch.tv/Amorisaiya]',
+        'commands': '!discord | !elo | !girth | !overkill | !rekt | !sens | !social | !subtember | !thanks | !uptime',
+        #'coolkids': 'Dane [twitch.tv/dhQk] | Jordan [twitch.tv/IncredFruityTV] | Leon [twitch.tv/LeoniLive] | Mitchell [twitch.tv/SenOokami] | MK [twitch.tv/Zexy_DemoN] | MyselfWhat [twitch.tv/MyselfWhat] | Rag [twitch.tv/Ragnarok1stx] | Sam [twitch.tv/SamKayNZ] | Sav [twitch.tv/Amorisaiya]',
         'discord': 'Come have some yarns with me at discord.gg/xGBFGZx',
         'elo': 'goo.gl/DUhU6j',
         #'emotes': 'leeeenLove leeeenKiss',
         'girth': 'I thought the Witcher card game was GIRTH instead of GWENT',
         'overkill': 'A tabletop RPG developed by Joseph Barber goo.gl/bmV3vq',
+        'rekt': '☐ Not rekt ☑ Rekt',
         'sens': '1600 dpi; 1.92 in game',
         'social': 'Get updates on my life at twitter.com/leeeennyy',
-        'yusss' : 'Thank you for all the support everyone! We made it to 100+ followers!',
-    }.get(x, 'This ain\'t a command. Please look at !commands')
+		'subtember': 'They\'re half price at the moment if you want to sub',
+		'thanks': 'Thank you for all the support everyone! We made it to 100+ followers!',
+        'uptime': uptime(),
+    }.get(x, 'This isn\'t a command. Please look at !commands')
 
 def main():
     ts = socket.socket()
@@ -61,12 +83,16 @@ def main():
                     command = get_search('(?<=!)[a-zA-Z]+', message)
 
                 if not (command == None):
-                    send_message(ts, '@' + username + ' ' + commands(command))
+                    if (command == 'rekt'):
+                        send_message(ts, commands(command))
+                    else:
+                        send_message(ts, '@' + username + ' ' + commands(command))
                 elif (message.lower() == 'hey'):
                     send_message(ts, 'Welcome to the stream @' + username + '!')
 
 def testshit():
-    message = '!aasdasd5475 asds'
+    message = '!uptime'
+    time.sleep(3);
     if (len(message.split(' ')) == 1):
         ping = get_search('(?<=!)[a-zA-Z]+', message)
     else:
